66ab99a8690d1342c9264856cd4728f6642cebd3
stefano gigli <stefanonihil@proton.me>
2025-03-28 00:57:51 +0100

    Add optional user input to set n_downloads

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def root():
 @authenticated
 def post_file():
     file = request.files["file"]
-
+    hp = request.files.get("n_downloads", config.healthbar)
     file_id = uuid4()
     with db.session() as s:
         s.add(
@@ -57,7 +57,7 @@ def post_file():
                 id = file_id,
                 filename = file.filename,
                 data = file.stream.read(),
-                n_downloads = 0
+                n_downloads = hp
             )
         )
         s.commit()
